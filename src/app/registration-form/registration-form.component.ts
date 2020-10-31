925ac410154135e36b86005fa97ccbc2bef2e2a9
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2020-10-31 23:46:28 +0530

    form validation completed

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,32 +1,34 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl, Form } from '@angular/forms';
+import { FormGroup, FormControl, Form, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 // import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-registration-form',
-  templateUrl: './registration-form.component2.html',
-  styleUrls: ['./registration-form.component2.css']
+  templateUrl: './registration-form.component.html',
+  styleUrls: ['./registration-form.component.css']
 })
 export class RegistrationFormComponent implements OnInit {
 
-  @Output() registrationFormInfo = new EventEmitter<FormGroup>();
-  // @Output() agreed = new EventEmitter();
+  private registrationForm: FormGroup;
+
   employeeInfo = [];
 
   constructor(private router: Router) { }
 
   ngOnInit() {
+    this.registrationForm = new FormGroup({
+      name : new FormControl('', Validators.required),
+      position : new FormControl('', Validators.required),
+      about : new FormControl('', Validators.required),
+      joiningDate : new FormControl('', Validators.required),
+    });
   }
 
-  registrationForm = new FormGroup({
-    userName : new FormControl(''),
-    position : new FormControl(''),
-    about : new FormControl(''),
-    joiningDate : new FormControl(''),
-  });
-
   onSubmit( ) {
+    if( !this.formValidate()){
+      return false;
+    }
     let employeeInfoInBrowser = localStorage.getItem('employeeInfoInBrowser');
     let employeeInfo = [];
     // console.log(employeeInfoInBrowser);
@@ -41,6 +43,13 @@ export class RegistrationFormComponent implements OnInit {
     }
     localStorage.setItem('employeeInfoInBrowser', JSON.stringify(employeeInfo));
     this.router.navigate(['/details']);
+  }
+
+  formValidate() {
+    let formValue = this.registrationForm.value
+    let allFieldsValid = formValue.name.length > 0 && formValue.position.length > 0
+                          formValue.aboutlength > 0 && formValue.joiningDate.length > 0 
+    return allFieldsValid;
   }
 
 }
