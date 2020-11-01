0df08c8bd9971afa23c132ff3f73189d35063065
Pankaj Yadav <pankaj.yadav.ece17@itbhu.ac.in>
2020-11-01 11:45:56 +0530

    changed appname in server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/workforce-employee'));
+app.use(express.static('./dist/registration-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/workforce-employee/'}),
+    res.sendFile('index.html', {root: 'dist/registration-app/'}),
 );
 
 app.listen(process.env.PORT || 8080);
